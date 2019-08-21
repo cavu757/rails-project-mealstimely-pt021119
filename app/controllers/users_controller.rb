0bef665adbbfad586a09cf26e1a388e6a081e916
cavu757 <jose.pehovazdiez@gmail.com>
2019-08-21 02:20:42 -0400

    User and Session controller edits
    write session create, destroy
    write user new, create, show
    write signin and logout HTTP verbs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+
+  before_action :user_new
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.alert = "Could not create user.  Please try to Sign Up again."
+      redirect_to root_path
+    end
+  end
+
+  def show
+    # binding.pry
+    @user = User.find(session[:user_id]) unless !User.exists?(id: session[:user_id])
+    if session[:user_id].to_s != params[:id] || session[:user_id].nil?
+      flash.alert = "User is not logged in. Click to Log In above."
+      session[:user_id] = nil
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :age, :password, :areucook)
+  end
+
+  def user_new
+    @user = User.new
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   resources :users
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+
   root 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
 end
