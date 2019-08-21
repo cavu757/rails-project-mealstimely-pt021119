aa1122825bc9f37e5abc041633b495ef53c418ec
cavu757 <jose.pehovazdiez@gmail.com>
2019-08-21 18:14:49 -0400

    corrected password confirmation params
    did not include previously

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :age, :password, :areucook)
+    params.require(:user).permit(:name, :email, :age, :password, :password_confirmation, :areucook)
   end
 
   def user_new
