80cfc31ac9362f20f645b730d14e3d75ad246eba
cavu757 <jose.pehovazdiez@gmail.com>
2019-08-31 05:29:49 -0400

    Update food.rb
    create scope method to get food for current user

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,7 @@ class Food < ApplicationRecord
   has_many :meals
   has_many :users, through: :meals
   has_many :comments, through: :meals
+
+  scope :my_food, -> (cook_id){where(cook_id: cook_id)}
+  
 end
